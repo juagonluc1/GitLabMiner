531481e2d757be3e6bd8e420f246629b3d8dd76d
Claudio Gonzalez Benito <clagonben@alum.us.es>
2023-04-30 20:13:51 +0200

    first commit

---
+++ src/main/java/AissProyect/GitLabMiner/GitLabMinerApplication.java
@@ -0,0 +1,13 @@
+package AissProyect.GitLabMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLabMinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AissProyect/GitLabMiner/GitLabMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package AissProyect.GitLabMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
